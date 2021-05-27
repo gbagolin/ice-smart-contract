a3197667c26af6cc51904dfbe074657d15d6da5c
Giovanni <giovannibagolin@outlook.it>
2021-05-27 13:42:05 +0200

    Removed sol test.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,15 @@
 const ICESmartContract = artifacts.require("ICESmartContract");
 
 contract("ICESmartContract", () => {
-  it("Company name should be equal", async () => {
+  it("Adding a new company", async () => {
     const iceContract = await ICESmartContract.deployed();
     await iceContract.addCompany("azienda di giovanni");
     const company = await iceContract.getCompanyById(0);
-    console.log(company.name);
-    assert.equal(company.name, 'azienda di giovanni', "names not");
+    assert.equal(company.name, "azienda di giovanni", "names not");
+  });
+  it("Company accessible also here", async () => {
+    const iceContract = await ICESmartContract.deployed();
+    const company = await iceContract.getCompanyById(0);
+    assert.equal(company.name, "azienda di giovanni", "names not");
   });
 });
